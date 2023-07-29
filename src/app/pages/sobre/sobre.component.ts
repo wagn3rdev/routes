3b19d4dfa44e2d7923a30d5f6c8ffdc24f04244e
wagner <wagncarv@outlook.com>
2023-07-29 15:02:40 -0300

    Rotas, redirecionamento com Typescript, navifgate

--- src/app/pages/sobre/sobre.component.ts
+++ src/app/pages/sobre/sobre.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-sobre',
@@ -10,7 +10,12 @@ export class SobreComponent implements OnInit {
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((res) => console.log(res));
     this.activatedRoute.queryParamMap.subscribe((res) => console.log(res));
+
+    setInterval(() => {
+      // this.router.navigate(['404']);
+      this.router.navigateByUrl('404');
+    }, 3000);
   }
 
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
 }
